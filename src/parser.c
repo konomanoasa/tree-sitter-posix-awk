2ad1b7dad650c019fa0b6fe20f44a68e913407b8
konomanoasa <238482287+konomanoasa@users.noreply.github.com>
2026-09-26 21:38:50 +0900

    chore: release 0.18.0

--- src/parser.c
+++ src/parser.c
@@ -170539,7 +170539,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_posix_awk(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 17,
+      .minor_version = 18,
       .patch_version = 0,
     },
   };
